cb61e47d051320d1d92f0d511e8cb3a5f88d7d36
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-04-05 14:34:23 -0300

    updated Index and App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { useState } from 'react';
 import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from "./components";
 
 
@@ -12,10 +11,10 @@ function App() {
 		<Box sx={{ backgroundColor: '#000' }}>
 			<Navbar />
 			<Routes>
-				<Route path="/" exact element={<Feed />}></Route>
+				<Route exact path="/" element={<Feed />}></Route>
 				<Route path="/video/:id" element={<VideoDetail />} ></Route>
 				<Route path="/channel/:id" element={<ChannelDetail />} ></Route>
-				<Route path="/search/:id" element={<SearchFeed />} ></Route>
+				<Route path="/search/:searchTerm" element={<SearchFeed />} ></Route>
 			</Routes>
 		</Box>
     </BrowserRouter>
