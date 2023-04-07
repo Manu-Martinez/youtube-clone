aba22c565474f8a139fdb573ccfb88000b704fc0
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-04-06 23:23:04 -0300

    fixing issue with channel details not loading

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -9,31 +9,39 @@ import { fetchFromAPI } from '../utils/fetchFromAPI'
 const ChannelDetail = () => {
 	const [channelDetail, setChannelDetail] = useState();
 	const [videos, setVideos] = useState(null);
+	
 	const { id } = useParams();
 
 	useEffect(() => {
-		fetchFromAPI(`channels?part=snippet&id=${id}`)
-		.then((data) => setChannelDetail(data?.items[0]));
-
-		fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
-		.then((data) => setVideos(data?.items[0]));
-	}, {id})
+		const fetchResults = async () => {
+		  const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+	
+		  setChannelDetail(data?.items[0]);
+	
+		  const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+	
+		  setVideos(videosData?.items);
+		};
+	
+		fetchResults();
+	  }, [id]);
 
   return (
 	<Box minHeight="95vh">
-		<Box>
-			<div style={{ background: 'rgb(63,94,251) radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)',
-			zIndex: 10,
-			height: '300px', }}></div>
-			<ChannelCard channelDetail={channelDetail} marginTop="-110px"></ChannelCard>
-		</Box>
-		<Box display="flex" p="2">
-			<Box sx={{ mr: {sm: '100px'} }}>
-			<Videos videos={videos}></Videos>
-			</Box>
-		</Box>
+	<Box>
+	  <div style={{
+		height:'300px',
+		background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+		zIndex: 10,
+	  }} />
+	  <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
 	</Box>
-  )
-}
+	<Box p={2} display="flex">
+	<Box sx={{ mr: { sm: '100px' } }}/>
+	  <Videos videos={videos} />
+	</Box>
+  </Box>
+);
+};
 
 export default ChannelDetail
